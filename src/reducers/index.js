ad0e932abd08d5912c0604c4ed534f62f0866345
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-10 13:57:16 -0600

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,46 @@
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL, ADD_SMURF } from "../actions";
 
 export const initialState = {
-}
+    smurfs: [],
+    isLoading: false,
+    error: ""
+};
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action)=>{
+    switch (action.type)
+    {
+        case FETCH_START:
+            return {
+                ...state,
+                smurfs: [],
+                isLoading: true,
+                error: ""
+            };
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                isLoading: false,
+                error: ""
+            };
+        case FETCH_FAIL:
+            return {
+                ...state,
+                smurfs: [],
+                isLoading: false,
+                error: action.payload
+            };
+        case ADD_SMURF:
+            return {
+                ...state,
+                smurfs: [...state.smurfs, action.payload],
+                isLoading: false,
+                error: ""
+            };
+        default:
+            return state;
+    }
+};
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
