45bcce4d30ec2085f58c5fa42ab9fcfb1ee19c53
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-10 14:13:52 -0600

    Update index.js
    part 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,41 +5,35 @@ export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAIL = "FETCH_FAIL";
 export const ADD_SMURF = "ADD_SMURF";
 
-export const fetchSmurfs = () =>
-{
-    return (dispatch =>
-    {
-        axios.get("http://localhost:3333/smurfs")
-            .then(resp =>
-            {
-                dispatch(fetchSmurfs(resp.data));
-            })
-            .catch(error =>
-            {
-                dispatch(fetchFail(error));
-            });
-    });
-};
+export const fetchSmurfs = () =>{
+    return(dispatch) =>{
+        dispatch(fetchStart());
 
-export const fetchStart = () =>
-{
-    return ({ type: FETCH_START });
-};
-
-export const fetchSuccesss = (smurfs) =>
-{
-    return ({ type: FETCH_SUCCESS, payload: smurfs });
-};
-
-export const fetchFail = (error) =>
-{
-    return ({ type: FETCH_FAIL, payload: error });
-};
-
-export const addSmurf = (smurf) => 
-{
-    return ({ type: ADD_SMURF, payload: smurf });
-};
+        axios
+        .get('http://localhost:3333/smurfs')
+        .then(res =>{
+            dispatch(fetchSuccess(res.data));
+        })
+        .catch(err =>{
+            dispatch(fetchFail("Couldn't get the info"))
+        })
+    }
+}
+export const fetchStart = () =>{
+    return({type:FETCH_START})
+}
+export const fetchSuccess = (smurfs) =>{
+    return({type:FETCH_SUCCESS, payload:smurfs})
+}
+export const fetchFail = (error) =>{
+    return({type:FETCH_FAIL, payload:error})
+}
+export const addSmurfs = (smurfs) =>{
+    return({type:ADD_SMURFS, payload:smurfs})
+}
+export const errorMessage = (error) =>{
+    return({type:ERROR, payload:error})
+}
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
