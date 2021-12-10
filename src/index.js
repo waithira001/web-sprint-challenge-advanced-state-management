982ef195019bb25c94df1f5794d0de33c4109bbb
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-10 13:57:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import reducer from './reducers/index';
+import { Provider } from 'react-redux';
+
+import {logger} from "redux-logger";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />, 
+        </Provider>,
     rootElement
 );
 
