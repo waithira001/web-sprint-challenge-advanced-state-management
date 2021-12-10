f40a49cb8df61d9e12ebebcd39094098c48b12e4
Ruth Muiruri <rmuiruri911@gmail.com>
2021-12-10 13:56:39 -0600

    Update App.js
    okay...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchSmurfs } from './actions';
+
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
@@ -7,20 +10,30 @@ import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
+    componentDidMount()
+    {
+        this.props.fetchSmurfs();
+    }
 
-      <main>
-        <SmurfList/>
-        <AddForm/>
-      </main>
-    </div>
-  );
+    render()
+    {
+      return (
+        <div className="App">
+          <Header />
+    
+          <main>
+            <SmurfList/>
+            <AddForm/>
+          </main>
+        </div>
+      );
+    }
+  
 }
+export default connect(null, { fetchSmurfs })(App);
 
-export default App;
+// export default App;
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
